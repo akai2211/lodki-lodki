0f302e1bd72fcf6a8d22e7f2adc5588a7137890f
GrigoriyLaptev <wipmoon@gmail.com>
2018-12-20 03:29:45 +0700

    Полностью все переверстано плюс сделано меню и категории header завершон

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  $('.humburger').on('click', function(e) {
+    e.preventDefault;
+    $(this).toggleClass('hum-active');
+    $('.menu').toggleClass('active');
+  });
+  
+  });
