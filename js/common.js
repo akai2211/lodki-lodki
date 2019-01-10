c465f23a30e61c2577960d1c0cde15a0193937e1
GrigoriyLaptev <wipmoon@gmail.com>
2019-01-11 06:44:48 +0700

    Баннер с приглошением в магазин

--- js/common.js
+++ js/common.js
@@ -5,5 +5,14 @@ $(document).ready(function() {
     $(this).toggleClass('hum-active');
     $('.menu').toggleClass('active');
   });
+
+  $('.sale-slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    arrows: false
+  });
   
   });
