7bdf4cbfcbdf7cff8f8fb622e034eff7d6a56a09
GrigoriyLaptev <wipmoon@gmail.com>
2019-01-19 07:53:34 +0700

    Закончина index страница

--- js/common.js
+++ js/common.js
@@ -14,5 +14,18 @@ $(document).ready(function() {
     speed: 500,
     arrows: false
   });
-  
+
+  $(window).scroll(function(){
+  if ($(this).scrollTop() > 100) {
+    $('.scrollup').fadeIn();
+    } else {
+    $('.scrollup').fadeOut();
+    }
+  });
+
+  $('.scrollup').click(function(){
+  $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+
   });
